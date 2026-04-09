ef5e8c7fdb99190d67e42ddac303c9dedc2413a1
carlosantonioverasayago <carlosantonio.verasayago@nazaretsanblas.org>
2026-04-09 09:23:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Esto ignora los errores de TypeScript para que te deje publicar
+    // ESTO ES LO IMPORTANTE: Ignora los errores para que te deje publicar
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Esto ignora los avisos de "ortografía" del código
+    // También ignoramos el linting por si acaso
     ignoreDuringBuilds: true,
   },
 }
