d5bd28961f5ea1402fe1fddfa26bf6f722e6e97c
carlosantonioverasayago <carlosantonio.verasayago@nazaretsanblas.org>
2026-04-09 08:47:58 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    // !! ADVERTENCIA !!
+    // Permite que los despliegues finalicen con éxito incluso si
+    // tu proyecto tiene errores de TypeScript.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignora los errores de ESLint durante el despliegue
+    ignoreDuringBuilds: true,
+  },
+}
 
-module.exports = nextConfig;
+export default nextConfig
